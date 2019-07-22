d1ff5774115fe176915020575005ccce5194ac7a
Dylan Skoda <dylan_skoda@live.com.au>
2019-07-23 08:29:07 +1000

    Fixed Console Error
    Fixed console error regarding the position of deltaTime

--- DylansMassiveRooster/Assets/Scripts/Player.cs
+++ DylansMassiveRooster/Assets/Scripts/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour
     
     [System.NonSerialized] private GameObject movePoint;
     [System.NonSerialized] private bool move = false;
-    [System.NonSerialized] private float timeToWait = 1.5f * Time.deltaTime;
+    [System.NonSerialized] private float timeToWait = 1.5f;
 
     private void Start()
     {
@@ -42,7 +42,7 @@ public class Player : MonoBehaviour
     
     private IEnumerator WaitForTime()
     {
-        yield return new WaitForSeconds(timeToWait);
+        yield return new WaitForSeconds(timeToWait * Time.deltaTime);
         Destroy(movePoint); // Destroy the movePoint
         move = false;// Stop Movement
     }
