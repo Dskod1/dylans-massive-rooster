d81dac56ad20fa79bf23b4bdf3ba20e3dd9a1269
Dylan Skoda <dylan_skoda@live.com.au>
2019-07-23 08:34:11 +1000

    Code Cleanup and Standardization

--- DylansMassiveRooster/Assets/Scripts/Player.cs
+++ DylansMassiveRooster/Assets/Scripts/Player.cs
@@ -1,26 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 1f;
+    [NonSerialized] private bool move;
+
+    [NonSerialized] private GameObject movePoint;
     [SerializeField] private GameObject movePointer;
+    [SerializeField] private float moveSpeed = 1f;
+    [NonSerialized] private readonly float timeToWait = 1.5f;
     
-    [System.NonSerialized] private GameObject movePoint;
-    [System.NonSerialized] private bool move = false;
-    [System.NonSerialized] private float timeToWait = 1.5f;
-
-    private void Start()
-    {
-    }
-
     private void Update()
     {
         Movement();
-
-        
-
-
     }
 
     private void Movement()
@@ -28,31 +21,27 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse0)) //Check if mouse has been clicked to move
         {
             Destroy(movePoint); // Destroy the previous movePoint before setting a new one
-            movePoint = Instantiate(movePointer, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);// Create an object where the mouse has been clicked
-            move = true;// Make movement possible
-            
+            movePoint = Instantiate(movePointer, Camera.main.ScreenToWorldPoint(Input.mousePosition),
+                Quaternion.identity); // Create an object where the mouse has been clicked
+            move = true; // Make movement possible
         }
 
-        if (move == true)// check if movement is possible
-        {
-            transform.position = Vector2.MoveTowards(transform.position, movePoint.transform.position, moveSpeed * Time.deltaTime);//Move towards the point clicked
-        }
-        
+        if (move) // check if movement is possible
+            transform.position = Vector2.MoveTowards(transform.position, movePoint.transform.position,
+                moveSpeed * Time.deltaTime); //Move towards the point clicked
     }
-    
+
     private IEnumerator WaitForTime()
     {
         yield return new WaitForSeconds(timeToWait * Time.deltaTime);
         Destroy(movePoint); // Destroy the movePoint
-        move = false;// Stop Movement
+        move = false; // Stop Movement
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.CompareTag("Pointer")) // Check if you have collided with the movement position
-        {
             StartCoroutine(
                 WaitForTime()); // Wait a little before stopping movement for player to get to center of movement
-        }
     }
 }
