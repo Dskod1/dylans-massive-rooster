6acdf04b0e08f368e7b7c5269ca2d91e04f07d1d
Dylan Skoda <dylan_skoda@live.com.au>
2019-07-24 07:55:40 +1000

    Movement Restricted
    -Increased the time of inactivity till move point is destroyed
    -Can no longer click out of the game area
    -Can now restrict where movement will go based on the Move Area object collider

---
+++ DylansMassiveRooster/Assets/Scripts/MoveArea.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveArea : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        FindObjectOfType<Player>().Movement();
+    }
+}
--- DylansMassiveRooster/Assets/Scripts/Player.cs
+++ DylansMassiveRooster/Assets/Scripts/Player.cs
@@ -9,14 +9,16 @@ public class Player : MonoBehaviour
     [NonSerialized] private GameObject movePoint;
     [SerializeField] private GameObject movePointer;
     [SerializeField] private float moveSpeed = 1f;
-    [NonSerialized] private readonly float timeToWait = 1.5f;
+    [NonSerialized] private readonly float timeToWait = 5f;
     
     private void Update()
     {
-        Movement();
+        if (move) // check if movement is possible
+            transform.position = Vector2.MoveTowards(transform.position, movePoint.transform.position,
+                moveSpeed * Time.deltaTime); //Move towards the point clicked
     }
 
-    private void Movement()
+    public void Movement()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0)) //Check if mouse has been clicked to move
         {
@@ -25,10 +27,6 @@ public class Player : MonoBehaviour
                 Quaternion.identity); // Create an object where the mouse has been clicked
             move = true; // Make movement possible
         }
-
-        if (move) // check if movement is possible
-            transform.position = Vector2.MoveTowards(transform.position, movePoint.transform.position,
-                moveSpeed * Time.deltaTime); //Move towards the point clicked
     }
 
     private IEnumerator WaitForTime()
@@ -38,7 +36,7 @@ public class Player : MonoBehaviour
         move = false; // Stop Movement
     }
 
-    private void OnTriggerEnter2D(Collider2D collider)
+    private void OnTriggerEnter2D(Collider2D collider) //Check for collision
     {
         if (collider.gameObject.CompareTag("Pointer")) // Check if you have collided with the movement position
             StartCoroutine(
