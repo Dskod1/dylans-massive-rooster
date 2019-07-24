8815dec471aad50979a62b19e7e0b201e32256f6
Dylan Skoda <dylan_skoda@live.com.au>
2019-07-24 13:25:29 +1000

    Changed Movement
    -Animated x will fade in and out to show move to location
    - On reaching x, it will continue to play until faded out
    - Animation will destroy its own game object
    - Changed move area to restricted areas
    - When player collides with restricted area player will stop movement

---
+++ DylansMassiveRooster/Assets/Scripts/Destroy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    [NonSerialized] public bool alive = true;
+
+    public void DestroyObject()
+    {
+        if (alive == false)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    public void SetToNotAlive()
+    {
+        alive = false;
+    }
+}
--- DylansMassiveRooster/Assets/Scripts/Player.cs
+++ DylansMassiveRooster/Assets/Scripts/Player.cs
@@ -13,33 +13,46 @@ public class Player : MonoBehaviour
     
     private void Update()
     {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0;
+        if (Input.GetKeyDown(KeyCode.Mouse0)) //Check if mouse has been clicked to move
+        {
+            Destroy(movePoint); // Destroy the previous movePoint before setting a new one
+            movePoint = Instantiate(movePointer, mousePos,
+                Quaternion.identity); // Create an object where the mouse has been clicked
+            move = true; // Make movement possible
+        }
         if (move) // check if movement is possible
             transform.position = Vector2.MoveTowards(transform.position, movePoint.transform.position,
                 moveSpeed * Time.deltaTime); //Move towards the point clicked
     }
 
-    public void Movement()
-    {
-        if (Input.GetKeyDown(KeyCode.Mouse0)) //Check if mouse has been clicked to move
-        {
-            Destroy(movePoint); // Destroy the previous movePoint before setting a new one
-            movePoint = Instantiate(movePointer, Camera.main.ScreenToWorldPoint(Input.mousePosition),
-                Quaternion.identity); // Create an object where the mouse has been clicked
-            move = true; // Make movement possible
-        }
-    }
-
     private IEnumerator WaitForTime()
     {
         yield return new WaitForSeconds(timeToWait * Time.deltaTime);
-        Destroy(movePoint); // Destroy the movePoint
+        FindObjectOfType<Destroy>().SetToNotAlive(); // Tells the x animation to destroy itself after it fades out
         move = false; // Stop Movement
     }
 
     private void OnTriggerEnter2D(Collider2D collider) //Check for collision
     {
         if (collider.gameObject.CompareTag("Pointer")) // Check if you have collided with the movement position
+        {
             StartCoroutine(
                 WaitForTime()); // Wait a little before stopping movement for player to get to center of movement
+            FindObjectOfType<Destroy>().GetComponent<Animator>().SetBool("pause", true);// Tells the animation to fade out and pause
+        }
+        if (collider.gameObject.CompareTag("Restricted")) // Check if you have collided with the movement position
+        {
+            StartCoroutine(
+                WaitForTime()); // Wait a little before stopping movement for player to get to center of movement
+            FindObjectOfType<Destroy>().GetComponent<Animator>().SetBool("pause", true);// Tells the animation to fade out and pause
+            move = false; // Stop Movement
+        }
     }
 }
