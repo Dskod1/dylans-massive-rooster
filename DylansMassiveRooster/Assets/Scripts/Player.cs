904b9cf09a0b72b07c0008177be648a41b88c528
Dylan Skoda <dylan_skoda@live.com.au>
2019-07-24 14:40:40 +1000

    Restricted Movement Fix
    - Game now uses the physics engine for movement
    - Prevents character from being able to pass through a collider

--- DylansMassiveRooster/Assets/Scripts/Player.cs
+++ DylansMassiveRooster/Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
 
     [NonSerialized] private GameObject movePoint;
     [SerializeField] private GameObject movePointer;
+    [SerializeField] private float reboundVelocity = 50f;
     [SerializeField] private float moveSpeed = 1f;
     [NonSerialized] private readonly float timeToWait = 5f;
     
@@ -27,9 +28,14 @@ public class Player : MonoBehaviour
                 Quaternion.identity); // Create an object where the mouse has been clicked
             move = true; // Make movement possible
         }
+
         if (move) // check if movement is possible
-            transform.position = Vector2.MoveTowards(transform.position, movePoint.transform.position,
-                moveSpeed * Time.deltaTime); //Move towards the point clicked
+        {
+            GetComponent<Rigidbody2D>().position = Vector2.MoveTowards(transform.position, movePoint.transform.position,
+             moveSpeed * Time.deltaTime); //Move towards the point clicked
+        }
+
+
     }
 
     private IEnumerator WaitForTime()
@@ -37,9 +43,10 @@ public class Player : MonoBehaviour
         yield return new WaitForSeconds(timeToWait * Time.deltaTime);
         FindObjectOfType<Destroy>().SetToNotAlive(); // Tells the x animation to destroy itself after it fades out
         move = false; // Stop Movement
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
     }
 
-    private void OnTriggerEnter2D(Collider2D collider) //Check for collision
+    private void  OnCollisionEnter2D(Collision2D collider) //Check for collision
     {
         if (collider.gameObject.CompareTag("Pointer")) // Check if you have collided with the movement position
         {
@@ -53,6 +60,23 @@ public class Player : MonoBehaviour
                 WaitForTime()); // Wait a little before stopping movement for player to get to center of movement
             FindObjectOfType<Destroy>().GetComponent<Animator>().SetBool("pause", true);// Tells the animation to fade out and pause
             move = false; // Stop Movement
+            Vector2 dir = collider.contacts[0].point -
+                          new Vector2(transform.position.x, transform.position.y); //get the direction of the collision
+            dir = -dir.normalized; // normalize direction and reverse it
+            GetComponent<Rigidbody2D>().AddForce(dir * reboundVelocity); // move player away from collision
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0); // stop player movement
         }
     }
+    private void  OnCollisionStay2D(Collision2D collider) //Check if colliders are stuck on each other
+    {
+        if (collider.gameObject.CompareTag("Restricted")) // Check if player is stuck on restricted collider
+        {
+            Vector2 dir = collider.contacts[0].point -
+                          new Vector2(transform.position.x, transform.position.y); //get the direction of the collision
+            dir = -dir.normalized; // normalize direction and reverse it
+            GetComponent<Rigidbody2D>().AddForce(dir * reboundVelocity); // move player away from collision
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0); // stop player movement
+        }
+    }
+
 }
