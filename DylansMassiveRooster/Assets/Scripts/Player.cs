ef6d2f8c010d0a948167be324011b58353725ce9
Dylan Skoda <dylan_skoda@live.com.au>
2019-07-25 08:20:20 +1000

    Minor Changes
    - Deleted Redundant Code
    - Set up the player to be animated

--- DylansMassiveRooster/Assets/Scripts/Player.cs
+++ DylansMassiveRooster/Assets/Scripts/Player.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [NonSerialized] private readonly float timeToWait = 10f;
+    [NonSerialized] private readonly float timeToWait = 1f;
     [NonSerialized] private bool move;
 
     [NonSerialized] private GameObject movePoint;
     [SerializeField] private GameObject movePointer;
     [SerializeField] private float moveSpeed = 1f;
+    private Rigidbody2D RB2D;
     [SerializeField] private float reboundVelocity = 50f;
+
+    private void Start()
+    {
+        RB2D = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
         Movement();
+        Debug.Log(RB2D.velocity);
     }
 
     // Player Movement
@@ -33,36 +40,38 @@ public class Player : MonoBehaviour
 
         if (move) // check if movement is possible
         {
-            GetComponent<Rigidbody2D>().position = Vector2.MoveTowards(transform.position, movePoint.transform.position,
+            RB2D.position = Vector2.MoveTowards(transform.position, movePoint.transform.position,
                 moveSpeed * Time.deltaTime); //Move towards the point clicked
         }
-        else if (FindObjectOfType<Destroy>() != null) //remove the move point
+        else
         {
-            FindObjectOfType<Destroy>().GetComponent<Animator>()
-                .SetBool("pause", true); // Tells the animation to fade out and pause
-            FindObjectOfType<Destroy>().SetToNotAlive(); // Tells the x animation to destroy itself after it fades out
+            if (FindObjectOfType<Destroy>() != null) //Checks if the move point still exsists
+            {
+                FindObjectOfType<Destroy>().GetComponent<Animator>()
+                    .SetBool("pause", true); // Tells the animation to fade out and pause
+                FindObjectOfType<Destroy>()
+                    .SetToNotAlive(); // Tells the x animation to destroy itself after it fades out
+            }
+
+            //Stop movement of character if move = false
+            RB2D.velocity = new Vector2(0, 0); // stop player movement
         }
     }
 
     private IEnumerator WaitForTime()
     {
         yield return new WaitForSeconds(timeToWait * Time.deltaTime);
-        move = false; // Stop Movement
-        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        move = false; // Stop Movement;
     }
 
     private void OnCollisionEnter2D(Collision2D collider) //Check for collision
     {
         if (collider.gameObject.CompareTag("Restricted")) // Check if you have collided with the movement position
         {
-            StartCoroutine(
-                WaitForTime()); // Wait a little before stopping movement for player to get to center of movement
-            move = false; // Stop Movement
             var dir = collider.contacts[0].point -
                       new Vector2(transform.position.x, transform.position.y); //get the direction of the collision
             dir = -dir.normalized; // normalize direction and reverse it
-            GetComponent<Rigidbody2D>().AddForce(dir * reboundVelocity); // move player away from collision
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0); // stop player movement
+            RB2D.AddForce(dir * reboundVelocity); // move player away from collision
             StartCoroutine(
                 WaitForTime()); // Wait a little before stopping movement for player to get to center of movement
         }
@@ -71,23 +80,21 @@ public class Player : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collider) //Check for collision trigger
     {
         if (collider.gameObject.CompareTag("Pointer")) // Check if you have collided with the movement position
-        {
-            StartCoroutine(
-                WaitForTime()); // Wait a little before stopping movement for player to get to center of movement
-        }
+            move = false; // Stop Movement
     }
 
-    private void OnCollisionStay2D(Collision2D collider) //Check if colliders are stuck on each other
+    private void OnCollisionStay2D(Collision2D collider) //Check if colliders are stuck on each other and correct
     {
         if (collider.gameObject.CompareTag("Restricted")) // Check if player is stuck on restricted collider
         {
             var dir = collider.contacts[0].point -
                       new Vector2(transform.position.x, transform.position.y); //get the direction of the collision
             dir = -dir.normalized; // normalize direction and reverse it
-            GetComponent<Rigidbody2D>().AddForce(dir * reboundVelocity); // move player away from collision
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0); // stop player movement
-            StartCoroutine(
-                WaitForTime()); // Wait a little before stopping movement for player to get to center of movement
+            RB2D.AddForce(dir * reboundVelocity); // move player away from collision
         }
     }
+
+    // Player Animation
+    // Animator Int "direction" 1 = up left; 2 = up right; 3 = down right; 4 = down left
+    // Animator bool "walk" 
 }
