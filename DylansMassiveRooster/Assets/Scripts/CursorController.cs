be9453c14f689d203e36d7f660edc400e0d5a53a
Dylan Skoda <dylan_skoda@live.com.au>
2019-07-29 09:31:40 +1000

    Item highlights and Cursor Changes
    - Branch item now highlights when hovering
    - Default cursor changed to walk
    - Cursor changes on hovering over item

---
+++ DylansMassiveRooster/Assets/Scripts/CursorController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CursorController : MonoBehaviour
+{
+    [SerializeField] public Texture2D cursorWalking;
+    [SerializeField] public Texture2D cursorExamine;
+    [SerializeField] public Texture2D cursorUse;
+    [SerializeField] public Texture2D cursorTake;
+    [SerializeField] public Texture2D cursorTalkTo;
+
+    public void changeToCursorWalking()
+    {
+        Cursor.SetCursor(cursorWalking, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void changeToCursorExamine()
+    {
+        Cursor.SetCursor(cursorExamine, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void changeToCursorUse()
+    {
+        Cursor.SetCursor(cursorUse, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void changeToCursorTake()
+    {
+        Cursor.SetCursor(cursorTake, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void changeToCursorTalkTo()
+    {
+        Cursor.SetCursor(cursorTalkTo, Vector2.zero, CursorMode.Auto);
+    }
+}
---
+++ DylansMassiveRooster/Assets/Scripts/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private void OnMouseOver()
+    {
+        GetComponent<Animator>()
+            .SetBool("mouseHovering", true);
+        FindObjectOfType<CursorController>().changeToCursorTake();
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<Animator>()
+            .SetBool("mouseHovering", false);
+        FindObjectOfType<CursorController>().changeToCursorWalking();
+    }
+}
