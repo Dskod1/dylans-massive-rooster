5769c4fcaad1f849d825a58dd07f4c42abb34b40
csprinks <csprinks@gmail.com>
2019-07-28 20:28:29 -0700

    Replaced dog graphic with new one, hooked up examine on dog but flash animation won't play for some reason I prob did not hook it up right

---
+++ DylansMassiveRooster/Assets/Scripts/Examine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Examine : MonoBehaviour
+{
+    private void OnMouseOver()
+    {
+        GetComponent<Animator>()
+            .SetBool("mouseHovering", true);
+        FindObjectOfType<CursorController>().changeToCursorExamine();
+    }
+
+    private void OnMouseExit()
+    {
+        GetComponent<Animator>()
+            .SetBool("mouseHovering", false);
+        FindObjectOfType<CursorController>().changeToCursorWalking();
+    }
+}
